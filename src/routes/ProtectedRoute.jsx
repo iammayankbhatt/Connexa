426702faec4fe138eca2f2920fc97d16c61795f5
Tanisha Bist <tanishabist3@gmail.com>
2026-04-27 12:33:49 +0530

    Refactor ProtectedRoute component for authentication

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
 
-const ProtectedRoute = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const ProtectedRoute = ({ children }) => {
+  const { user } = useAuth();
+  if (!user) return <Navigate to="/" replace />;
+  return children;
+};
 
-export default ProtectedRoute
+export default ProtectedRoute;
